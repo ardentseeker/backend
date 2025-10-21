85919e9ee5a1a18644fa58f1e0922eb03a2a6a34
Roushangopal <roushanraja0@gmail.com>
2025-10-21 16:41:53 +0530

    Resolve errors

--- server.js
+++ server.js
@@ -40,7 +40,4 @@ app.get("/", (req, res) => {
 
 app.use("/api/contacts", contactRoutes);
 
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
-
 export default app;
